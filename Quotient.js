6bdca2866d3ec00bc0d609be9ece2e767d910281
w2j1y12 <62472117+w2j1y12@users.noreply.github.com>
2023-06-15 10:27:24 +0900

    Create Quotient.js

---
+++ Quotient.js
@@ -0,0 +1,15 @@
+/*
+<문제 설명>
+정수 num1, num2가 매개변수로 주어질 때, num1을 num2로 나눈 몫을 return 하도록 solution 함수를 완성해주세요.
+
+<제한사항>
+0 < num1 <= 100
+0 < num2 <= 100
+*/
+
+function solution(num1, num2) {
+    return Math.floor(num1/num2);
+}
+
+console.log(solution(10,5));
+console.log(solution(7,2));
