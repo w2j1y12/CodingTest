3038f337d31cdf7c6ce432cf893380f26b00dcf7
w2j1y12 <62472117+w2j1y12@users.noreply.github.com>
2023-06-15 10:31:36 +0900

    Create nDivision.js

---
+++ nDivision.js
@@ -0,0 +1,22 @@
+/* 
+<문제 설명>
+정수 num1과 num2가 매개변수로 주어질 때, num1을 num2로 나눈 값에 1,000을 곱한 후 정수 부분을 return 하도록 soltuion 함수를 완성해주세요.
+
+<제한사항>
+0 < num1 ≤ 100
+0 < num2 ≤ 100
+*/
+
+function solution(num1, num2) {
+    return Math.floor((num1/num2)*1000);
+}
+
+console.log(solution(3,2));
+console.log(solution(7,3));
+console.log(solution(1,16));
+
+// 다른 사람 풀이
+function solution(num1, num2) {
+    return Math.trunc(num1 / num2 * 1000);
+}
+// Math.trunc() 소수점 이하는 다 버림
