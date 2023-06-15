c41544e7afc4fecdc59b8a602a551acd64d14296
w2j1y12 <62472117+w2j1y12@users.noreply.github.com>
2023-06-15 10:45:45 +0900

    Create doubleArr.js

---
+++ doubleArr.js
@@ -0,0 +1,17 @@
+/*
+<문제 설명>
+정수 배열 numbers가 매개변수로 주어집니다.
+numbers의 각 원소에 두배한 원소를 가진 배열을 return하도록 solution 함수를 완성해주세요.
+
+<제한사항>
+-10,000 ≤ numbers의 원소 ≤ 10,000
+1 ≤ numbers의 길이 ≤ 1,000
+*/
+
+function solution(numbers) {
+    var answer = [];
+    for(let i=0; i < numbers.length; i++) {
+        answer[i] = 2*numbers[i];
+    }
+    return answer;
+}
